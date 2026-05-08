f9903da0a8106adb845decc6fa71eb11b32c972e
jinwoo8.hong <nonombb@naver.com>
2026-05-08 16:39:43 +0900

    [Description] Disable QuickEdit mode to prevent console freeze on click [Type] bugfix [Developer] jinwoo8.hong

--- SampleOrderSystem/SampleOrderSystem/SampleOrderSystem.cpp
+++ SampleOrderSystem/SampleOrderSystem/SampleOrderSystem.cpp
@@ -2,9 +2,17 @@
 #include "Controllers/AppController.h"
 
 int main() {
+    // QuickEdit 모드 비활성화 - 콘솔 클릭 시 I/O가 멈추는 현상 방지
+    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD consoleMode;
+    GetConsoleMode(hInput, &consoleMode);
+    consoleMode &= ~ENABLE_QUICK_EDIT_MODE;
+    consoleMode |= ENABLE_EXTENDED_FLAGS;
+    SetConsoleMode(hInput, consoleMode);
+
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
-    FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
+    FlushConsoleInputBuffer(hInput);
 
     AppController app;
     app.Run();
